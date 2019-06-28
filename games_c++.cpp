58c83f3bec2019f28bf49acb84cb1bda5e66c194
Eugene S <admin@biptik.com>
2019-06-29 07:03:10 +1000

    Guess number init

--- games_c++.cpp
+++ games_c++.cpp
@@ -12,7 +12,22 @@ int main()
 
 	std::cout << "\welcome to Guess My Number\n\n";
 
-	//Gane cycle
-	std::cout << "Enter a guess: ";
-}
+	//Gamee cycle
+	do {
+		std::cout << "Enter a guess: ";
+		std::cin >> guess;
+		++tries;
 
+		if (guess > secretNumber) {
+			std::cout << "Too high\n\n";
+		}
+		else if (guess < secretNumber) {
+			std::cout << "Too low\n\n";
+		}
+		else {
+			std::cout << "\nThat is it! You got it in " << tries << " guesses!\n";
+		}
+	} while (guess != secretNumber);
+	
+	return 0;
+}
