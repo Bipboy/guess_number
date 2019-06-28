6f8ca585cefa78679414e4cd33d22dfe16382419
Eugene S <admin@biptik.com>
2019-06-29 06:17:04 +1000

    Add project files.

---
+++ games_c++.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(0)));
+	int secretNumber = rand() % 100 + 1;
+	int tries = 0, guess;
+
+	std::cout << "\welcome to Guess My Number\n\n";
+
+	//Gane cycle
+	std::cout << "Enter a guess: ";
+}
+
